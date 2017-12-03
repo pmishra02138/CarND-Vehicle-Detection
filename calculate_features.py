bc8c6fd1084d2851a5d642d034da330e6add26ea
Pankaj Mishra <pmishra02138@gmail.com>
2017-12-02 19:04:52 -0800

    Write color conversion function

--- calculate_features.py
+++ calculate_features.py
@@ -12,21 +12,29 @@ from skimage.feature import hog
 from sklearn.model_selection import train_test_split
 # from sklearn.cross_validation import train_test_split
 
+def convert_color(img, conv='RGB2YCrCb'):
+    if conv == 'RGB2YCrCb':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    if conv == 'BGR2YCrCb':
+        return cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    if conv == 'RGB2LUV':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+
 # Define a function to do color conversion
-def color_conversion(image, cspace='RGB'):
+def color_conversion(img, cspace='RGB'):
     # apply color conversion if other than 'RGB'
     if cspace != 'RGB':
         if cspace == 'HSV':
-            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
         elif cspace == 'LUV':
-            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
         elif cspace == 'HLS':
-            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
         elif cspace == 'YUV':
-            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
         elif cspace == 'YCrCb':
-            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
-    else: feature_image = np.copy(image)
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    else: feature_image = np.copy(img)
 
     return feature_image
 
@@ -73,9 +81,9 @@ def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
     features = []
     # feature_array
     # Extract features for all images
-    for file in imgs:
+    for img in imgs:
         # Read in each one by one
-        image = mpimg.imread(file)
+        image = mpimg.imread(img)
 
         # 1) Apply color conversion if other than 'RGB'
         feature_image = color_conversion(image, cspace='RGB')
@@ -103,71 +111,72 @@ def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
     # Return list of feature vectors
     return features
 
+if __name__ == '__main__':
 
-# Divide up into cars and notcars
-images = glob.glob('data/*/*/*.png')
-cars = []
-notcars = []
-for image in images:
-    if 'non-vehicles' in image:
-        notcars.append(image)
-    else:
-        cars.append(image)
+    # Divide up into cars and notcars
+    images = glob.glob('data/*/*/*.png')
+    cars = []
+    notcars = []
+    for image in images:
+        if 'non-vehicles' in image:
+            notcars.append(image)
+        else:
+            cars.append(image)
 
-# Reduce the sample size because HOG features are slow to compute
-# The quiz evaluator times out after 13s of CPU time
-sample_size = 500
-cars = cars[0:sample_size]
-notcars = notcars[0:sample_size]
+    # Reduce the sample size because HOG features are slow to compute
+    # The quiz evaluator times out after 13s of CPU time
+    sample_size = 500
+    cars = cars[0:sample_size]
+    notcars = notcars[0:sample_size]
 
-### Tweak these parameters and see how the results change.
-colorspace = 'RGB' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-orient = 9
-pix_per_cell = 8
-cell_per_block = 2
-hog_channel = 0 # Can be 0, 1, 2, or "ALL"
+    ### Tweak these parameters and see how the results change.
+    colorspace = 'RGB' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+    orient = 9
+    pix_per_cell = 8
+    cell_per_block = 2
+    hog_channel = 0 # Can be 0, 1, 2, or "ALL"
 
-t=time.time()
-car_features = extract_features(cars, cspace=colorspace, orient=orient,
-                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
-                        hog_channel=hog_channel)
-notcar_features = extract_features(notcars, cspace=colorspace, orient=orient,
-                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
-                        hog_channel=hog_channel)
-t2 = time.time()
-print(round(t2-t, 2), 'Seconds to extract HOG features...')
-# Create an array stack of feature vectors
-print(np.asarray(car_features).shape,np.asarray(notcar_features).shape)
-X = np.vstack((np.asarray(car_features), np.asarray(notcar_features))).astype(np.float64)
-# Fit a per-column scaler
-X_scaler = StandardScaler().fit(X)
-# Apply the scaler to X
-scaled_X = X_scaler.transform(X)
+    t=time.time()
+    car_features = extract_features(cars, cspace=colorspace, orient=orient,
+                            pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                            hog_channel=hog_channel)
+    notcar_features = extract_features(notcars, cspace=colorspace, orient=orient,
+                            pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                            hog_channel=hog_channel)
+    t2 = time.time()
+    print(round(t2-t, 2), 'Seconds to extract HOG features...')
+    # Create an array stack of feature vectors
+    print(np.asarray(car_features).shape,np.asarray(notcar_features).shape)
+    X = np.vstack((np.asarray(car_features), np.asarray(notcar_features))).astype(np.float64)
+    # Fit a per-column scaler
+    X_scaler = StandardScaler().fit(X)
+    # Apply the scaler to X
+    scaled_X = X_scaler.transform(X)
 
-# Define the labels vector
-y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+    # Define the labels vector
+    y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
 
-# Split up data into randomized training and test sets
-rand_state = np.random.randint(0, 100)
-X_train, X_test, y_train, y_test = train_test_split(
-    scaled_X, y, test_size=0.2, random_state=rand_state)
+    # Split up data into randomized training and test sets
+    rand_state = np.random.randint(0, 100)
+    X_train, X_test, y_train, y_test = train_test_split(
+        scaled_X, y, test_size=0.2, random_state=rand_state)
 
-print('Using:',orient,'orientations',pix_per_cell,
-    'pixels per cell and', cell_per_block,'cells per block')
-print('Feature vector length:', len(X_train[0]))
-# Use a linear SVC
-svc = LinearSVC()
-# Check the training time for the SVC
-t=time.time()
-svc.fit(X_train, y_train)
-t2 = time.time()
-print(round(t2-t, 2), 'Seconds to train SVC...')
-# Check the score of the SVC
-print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
-# Check the prediction time for a single sample
-t=time.time()
-n_predict = 10
-print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
-print('For these',n_predict, 'labels: ', y_test[0:n_predict])
-t2 = time.time()
-print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
+    print('Using:',orient,'orientations',pix_per_cell,
+        'pixels per cell and', cell_per_block,'cells per block')
+    print('Feature vector length:', len(X_train[0]))
+    # Use a linear SVC
+    svc = LinearSVC()
+    # Check the training time for the SVC
+    t=time.time()
+    svc.fit(X_train, y_train)
+    t2 = time.time()
+    print(round(t2-t, 2), 'Seconds to train SVC...')
+    # Check the score of the SVC
+    print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+    # Check the prediction time for a single sample
+    t=time.time()
+    n_predict = 10
+    print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+    print('For these',n_predict, 'labels: ', y_test[0:n_predict])
+    t2 = time.time()
+    print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
