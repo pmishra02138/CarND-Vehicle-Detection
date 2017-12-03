6a7ba8d7a4a79d1e6457c7703558c1766ff74bce
Pankaj Mishra <pmishra02138@gmail.com>
2017-12-02 17:12:19 -0800

    Calculate hog, spatial and color features

--- image_geatures.py
+++ image_geatures.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import train_test_split
 # from sklearn.cross_validation import train_test_split
 
 # Define a function to do color conversion
-def color_conversion(img, cspace='RGB'):
+def color_conversion(image, cspace='RGB'):
     # apply color conversion if other than 'RGB'
     if cspace != 'RGB':
         if cspace == 'HSV':
@@ -29,6 +29,24 @@ def color_conversion(img, cspace='RGB'):
     else: feature_image = np.copy(image)
 
     return feature_image
+
+# Define a function to compute binned color features
+def bin_spatial(img, size=(32, 32)):
+    # Use cv2.resize().ravel() to create the feature vector
+    features = cv2.resize(img, size).ravel()
+    # Return the feature vector
+    return features
+
+# Define a function to compute color histogram features
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    # Compute the histogram of the color channels separately
+    channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+    channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+    channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return hist_features
 
 # Define a function to return HOG features and visualization
 def get_hog_features(img, orient, pix_per_cell, cell_per_block,
@@ -48,20 +66,27 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
 
 
 # Define a function to extract features from a list of images
-# Have this function call bin_spatial() and color_hist()
-def extract_features(imgs, cspace='RGB', orient=9,
+def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
+                        hist_bins=32, orient=9,
                         pix_per_cell=8, cell_per_block=2, hog_channel=0):
-    # Create a list to append feature vectors to
+    # Create an empty features list
     features = []
-
-    # Color conversion
+    # feature_array
+    # Extract features for all images
     for file in imgs:
         # Read in each one by one
         image = mpimg.imread(file)
-        # apply color conversion if other than 'RGB'
+
+        # 1) Apply color conversion if other than 'RGB'
         feature_image = color_conversion(image, cspace='RGB')
 
-        # Call get_hog_features() with vis=False, feature_vec=True
+        # # 2) Compute spatial features if flag is set
+        spatial_features = bin_spatial(feature_image, size=spatial_size)
+
+        # 3) Compute histogram features if flag is set
+        hist_features = color_hist(feature_image, nbins=hist_bins)
+
+        # 4) Compute hog features: get_hog_features() with vis=False, feature_vec=True
         if hog_channel == 'ALL':
             hog_features = []
             for channel in range(feature_image.shape[2]):
@@ -74,7 +99,7 @@ def extract_features(imgs, cspace='RGB', orient=9,
                         pix_per_cell, cell_per_block, vis=False, feature_vec=True)
 
         # Append the new feature vector to the features list
-        features.append(hog_features)
+        features.append(np.concatenate((hog_features, hist_features, spatial_features)))
     # Return list of feature vectors
     return features
 
@@ -91,9 +116,9 @@ for image in images:
 
 # Reduce the sample size because HOG features are slow to compute
 # The quiz evaluator times out after 13s of CPU time
-# sample_size = 500
-# cars = cars[0:sample_size]
-# notcars = notcars[0:sample_size]
+sample_size = 500
+cars = cars[0:sample_size]
+notcars = notcars[0:sample_size]
 
 ### Tweak these parameters and see how the results change.
 colorspace = 'RGB' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
@@ -112,7 +137,8 @@ notcar_features = extract_features(notcars, cspace=colorspace, orient=orient,
 t2 = time.time()
 print(round(t2-t, 2), 'Seconds to extract HOG features...')
 # Create an array stack of feature vectors
-X = np.vstack((car_features, notcar_features)).astype(np.float64)
+print(np.asarray(car_features).shape,np.asarray(notcar_features).shape)
+X = np.vstack((np.asarray(car_features), np.asarray(notcar_features))).astype(np.float64)
 # Fit a per-column scaler
 X_scaler = StandardScaler().fit(X)
 # Apply the scaler to X
@@ -120,7 +146,6 @@ scaled_X = X_scaler.transform(X)
 
 # Define the labels vector
 y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
-
 
 # Split up data into randomized training and test sets
 rand_state = np.random.randint(0, 100)
